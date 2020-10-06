f01c8be4ccc205ab9a6002dc9d869b2739365195
yash <yashramoliya12@gmail.com>
2020-10-06 10:30:31 -0700

    add min and max problem

---
+++ Trees/BinaryTree/MinMax/minMax.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+#include"../BinaryTree.h"
+
+class Pair {
+    public:
+    int min;
+    int max;
+};
+
+Pair minMax(BinaryTreeNode<int>* root) {
+    if(root == NULL) {
+        Pair ans;
+        ans.max = 0;
+        ans.min = INT_MAX;
+        return ans;
+    }
+
+    Pair leftAns {minMax(root->left)};
+    Pair rightAns {minMax(root->right)};
+
+    int max {0};
+    int min {INT_MAX};
+    if(leftAns.max > rightAns.max) max = leftAns.max;
+    else max = rightAns.max;
+
+    if(leftAns.min < rightAns.min) min = leftAns.min;
+    else min = rightAns.min;
+
+    if(root->data > max) max = root->data;
+    if(root->data < min) min = root->data;
+
+    Pair ans;
+    ans.max = max;
+    ans.min = min;
+    return ans; 
+}
+
+int main() {
+    BinaryTreeNode<int>* root {TakeInputLevelWise()};
+    Pair ans {minMax(root)};
+    cout << ans.max << " " << ans.min << endl; 
+}   
---
+++ Trees/BinaryTree/MinMax/readme.md
@@ -0,0 +1,12 @@
+## Min and Max of Binary Tree
+Given a binary tree, find and return the min and max data value of given binary tree.
+Return the output as an object of PairAns class, which is already created.
+### Input format :
+Elements in level order form (separated by space)
+(If any node does not have left or right child, take -1 in its place)
+### Output Format :
+Max and min (separated by space)
+### Sample Input :
+8 3 10 1 6 -1 14 -1 -1 4 7 13 -1 -1 -1 -1 -1 -1 -1
+### Sample Output :
+14 1
