770071a30b86b431c3ff280635f561393e98a154
Yash <yashramoliya12@gmail.com>
2020-10-21 14:12:19 +0530

    add find path to root

---
+++ BinarySearchTree/FindPathInBST/findPath.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+#include"../BST.h"
+
+
+vector<int>* RootToNodePath(BinarySearchTreeNode<int>* root, int x) {
+    if(root == NULL) return NULL;
+
+    if(root->data == x) {
+        vector<int>* v {new vector<int>};
+        v->push_back(x);
+        return v;
+    } 
+
+    vector<int>* ans {RootToNodePath(root->left,x)};
+    if(ans != NULL) {
+        ans->push_back(root->data);
+        return ans;
+    }
+
+    vector<int>* rans {RootToNodePath(root->right,x)};
+    if(rans != NULL) {
+        rans->push_back(root->data);
+        return rans;
+    }
+    return NULL;
+}
+
+int main() {
+    BinarySearchTreeNode<int>* root {takeInput()};
+    int x;
+    cin >> x;
+    vector<int>* v {RootToNodePath(root,x)};
+    for(int i{}; i<v->size(); i++) {
+        cout << v->at(i) << " ";
+    }
+}
