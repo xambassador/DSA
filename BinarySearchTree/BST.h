fe229cfa41ec5d8eb1a6fa6c090efd51570efb49
Yash <yashramoliya12@gmail.com>
2020-10-23 19:18:14 +0530

    add print level wise function in BST.h

--- BinarySearchTree/BST.h
+++ BinarySearchTree/BST.h
@@ -1,6 +1,27 @@
 #include<queue>
 #include"BinarySearchTree.h"
 
+void printLevelWise(BinarySearchTreeNode<int>* root) {
+    if(root == NULL) return;
+    queue<BinarySearchTreeNode<int>* > pendingNodes;
+    pendingNodes.push(root);
+    pendingNodes.push(NULL);
+    while(!pendingNodes.empty()) {
+        BinarySearchTreeNode<int>* front {pendingNodes.front()};
+        pendingNodes.pop();
+        if(front == NULL) {
+            if(pendingNodes.empty()) break;
+            cout << endl;
+            pendingNodes.push(NULL);
+            continue;
+        }
+        cout << front->data << " ";
+        if(front->left != NULL) pendingNodes.push(front->left);
+        if(front->right != NULL) pendingNodes.push(front->right);
+    }
+}
+
+
 void print(BinarySearchTreeNode<int>* root) {
     if(root == NULL) return;
     cout << root->data << ": ";
