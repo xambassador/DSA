c622ee7cbb505f2ed18d4698c14cdea0cb7cc9d6
Yash <yashramoliya12@gmail.com>
2020-10-26 13:21:52 +0530

    add new problem - largest bst

---
+++ BinarySearchTree/LargestBST/largestBST.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+#include"../BST.h"
+
+
+class Pair {
+    public:
+    int height;
+    int max;
+    int min;
+    bool isBST;
+};
+
+
+Pair help(BinarySearchTreeNode<int>* root) {
+    if(root == NULL) {
+        Pair ans;
+        ans.height = 0;
+        ans.isBST = true;
+        ans.max = INT_MIN;
+        ans.min = INT_MAX;
+        return ans;
+    }
+
+    if(root->left == NULL && root->right == NULL) {
+        Pair ans;
+        ans.height = 1;
+        ans.isBST = true;
+        ans.max = root->data;
+        ans.min = root->data;
+        return ans;
+    }
+
+    Pair left {help(root->left)};
+    Pair right {help(root->right)};
+
+    Pair ans;
+
+    // if root is bst
+    bool isBST {left.max <= root->data && right.min >= root->data && left.isBST && right.isBST};
+    if(isBST) {
+        ans.height = 1 + max(left.height, right.height);
+        ans.isBST = true;
+        ans.max = max(root->data, max(left.max, right.max));
+        ans.min = min(root->data, min(left.min, right.min));
+    }else{
+        ans.height = max(left.height, right.height);
+        ans.isBST = false;
+        ans.max = max(root->data, max(left.max, right.max));
+        ans.min = min(root->data, min(left.min, right.min));
+    }
+    return ans;
+}
+
+
+int largestBST(BinarySearchTreeNode<int>* root)  {
+    Pair ans {help(root)};
+    return ans.height;
+}
+
+int main() {
+    BinarySearchTreeNode<int>* root {takeInput()};
+    int ans {largestBST(root)};
+    cout << ans << endl;
+}
---
+++ BinarySearchTree/LargestBST/readme.md
@@ -0,0 +1,10 @@
+## Largest BST
+Given a Binary tree, find the largest BST subtree. That is, you need to find the BST with maximum height in the given binary tree.
+Return the height of largest BST.
+### Input format :
+Line 1 : Elements in level order form (separated by space)<br/>
+(If any node does not have left or right child, take -1 in its place)
+### Sample Input 1:
+5 10 6 2 3 -1 -1 -1 -1 -1 9 -1 -1
+### Sample Output 1:
+2
