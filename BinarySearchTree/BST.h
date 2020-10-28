5f0b426bbb9c08ac6118dc33894a51b5afb2f144
Yash <yashramoliya12@gmail.com>
2020-10-28 13:14:12 +0530

    add pre, post and inorder traversal

--- BinarySearchTree/BST.h
+++ BinarySearchTree/BST.h
@@ -1,6 +1,28 @@
 #include<queue>
 #include"BinarySearchTree.h"
 
+void postOrder(BinarySearchTreeNode<int>* root) {
+    if(root == NULL) return;
+    postOrder(root->left);
+    postOrder(root->right);
+    cout << root->data << " ";
+}
+
+void inOrder(BinarySearchTreeNode<int>* root) {
+    if(root == NULL) return;
+    inOrder(root->left);
+    cout << root->data << " ";
+    inOrder(root->right);
+}
+
+
+void preOrder(BinarySearchTreeNode<int>* root) {
+    if(root == NULL) return;
+    cout << root->data << " ";
+    preOrder(root->left);
+    preOrder(root->right);
+}
+
 void printLevelWise(BinarySearchTreeNode<int>* root) {
     if(root == NULL) return;
     queue<BinarySearchTreeNode<int>* > pendingNodes;
