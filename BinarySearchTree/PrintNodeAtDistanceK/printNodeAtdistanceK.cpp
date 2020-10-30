bebbc4bb5137ffa6551d3b3e322227e84c0e931a
Yash <yashramoliya12@gmail.com>
2020-10-30 13:58:45 +0530

    add new problem - Print node at distance k

---
+++ BinarySearchTree/PrintNodeAtDistanceK/printNodeAtdistanceK.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+#include"../BST.h"
+
+
+void printNodesAtDepthK(BinarySearchTreeNode<int>* root, int k) {
+    if(root == NULL || k < 0) return;
+
+    if(k == 0) {
+        cout << root->data << " ";
+        return;
+    }
+
+    printNodesAtDepthK(root->left, k-1);
+    printNodesAtDepthK(root->right, k-1);
+}
+
+
+int printAtDistanceK(BinarySearchTreeNode<int>* root, int k, int element) {
+    if(root == NULL) return -1;
+
+    if(root->data == element) {
+        printNodesAtDepthK(root,k);
+        return 0;
+    }
+
+    int leftDistance {printAtDistanceK(root->left, k, element)};
+    if(leftDistance != -1) {
+        if(leftDistance + 1 == k) {
+            cout << root->data << " ";
+        }else{
+            printNodesAtDepthK(root->right,k-leftDistance-2);
+        }
+        return leftDistance+1;
+    }
+
+    int rightDistance {printAtDistanceK(root->right, k,element)};
+    if(rightDistance == -1) {
+        if(rightDistance + 1 == k) {
+            cout << root->data << " ";
+        }else{
+            printNodesAtDepthK(root->left,k-rightDistance-2);
+        }
+        return rightDistance+1;
+    }
+    return -1;
+}
+
+void printAtKDistance(BinarySearchTreeNode<int>* root, int k, int element){
+    printAtDistanceK(root,k,element);
+}
+
+
+int main() {
+    BinarySearchTreeNode<int>* root {takeInput()};
+    int k;
+    cin >> k;
+    int element;
+    cin >> element;
+    printAtDistanceK(root, k, element);
+}   
---
+++ BinarySearchTree/PrintNodeAtDistanceK/readme.md
@@ -0,0 +1,16 @@
+## Print nodes at distance k from node
+Given a binary tree, a node and an integer K, print nodes which are at K distance from the the given node.
+### Input format :
+Line 1 : Elements in level order form (separated by space)<br/>
+(If any node does not have left or right child, take -1 in its place)<br/>
+Line 2 : Node<br/>
+Line 3 : K<br/>
+### Output format : 
+Answer nodes in different line
+### Sample Input :
+5 6 10 2 3 -1 -1 -1 -1 -1 9 -1 -1<br/>
+3<br/>
+1
+### Sample Output :
+9<br/>
+6
