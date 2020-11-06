6d1a72954caaaf85acd3dbe8860dc8cb9289961f
Yash <yashramoliya12@gmail.com>
2020-11-05 22:11:26 -0800

    fix function

--- Recursion-And-Backtracking/RateInMaze/rateInMaze.cpp
+++ Recursion-And-Backtracking/RateInMaze/rateInMaze.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 
-int ans[11][11];
+int ans[20][20];
 
 
 void print(int n) {
@@ -14,7 +14,7 @@ void print(int n) {
     }cout << endl;
 }
 
-void help(int maze[][11], int n, int row, int col) {
+void help(int maze[][20], int n, int row, int col) {
     if(row == n-1 && col == n-1) {
         ans[row][col] = 1;
         print(n);
@@ -22,35 +22,22 @@ void help(int maze[][11], int n, int row, int col) {
         return;
     }
 
-    if(maze[row][col] == 0) return;
+    if(maze[row][col] == 0 || ans[row][col] == 1 || row < 0 || col < 0 || row >= n || col >= n) {
+        return;
+    }
 
     ans[row][col] = 1;
-    if(col+1<n && ans[row][col+1] != 1 && maze[row][col] != 0) {
-        help(maze,n,row,col+1);
-        ans[row][col+1] = 0;   
-    }
-
-    if(col-1>=0 && ans[row][col-1] != 1 && maze[row][col-1] != 0) {
-        help(maze,n,row,col-1);
-        ans[row][col-1] = 0;   
-    }
-
-    if(row+1<n && ans[row+1][col] != 1 && maze[row+1][col] != 0) {
-        help(maze,n,row+1,col);
-        ans[row+1][col] = 0;   
-    }
-
-    if(row-1>=0 && ans[row-1][col] != 1 && maze[row-1][col] != 0) {
-        help(maze,n,row-1,col);
-        ans[row][col+1] = 0;   
-    }
-
+    help(maze,n,row-1,col);
+    help(maze,n,row+1,col);
+    help(maze,n,row,col+1);
+    help(maze,n,row,col-1);
     ans[row][col] = 0;
+    return;
 }
 
 
-void rateInMaze(int maze[][11], int n) {
-    memset(ans, 0, 11*11*sizeof(int));
+void rateInMaze(int maze[][20], int n) {
+    memset(ans, 0, 20*20*sizeof(int));
     help(maze, n, 0,0);
 }
 
@@ -58,7 +45,7 @@ void rateInMaze(int maze[][11], int n) {
 int main() {
     int n;
     cin >> n;
-    int maze[11][11];
+    int maze[20][20];
     for(int i=0; i<n; i++) {
         for(int j=0; j<n; j++) {
             cin >> maze[i][j];
