0c7fb08e8d26efec28900c78ad489b7b39e9693f
Yash Ramoliya <yashramoliya12@gmail.com>
2020-12-15 14:15:04 +0530

    add knapsack problem solution

---
+++ DynammicProgramming/Knapsack/knapsack.cpp
@@ -0,0 +1,86 @@
+#include<iostream>
+
+// Brute force
+int knapsackHelp(int* w, int* v, int W, int start, int end) {
+    if(W == 0) {
+        return 0;
+    }
+
+    if(start > end) {
+        return 0;
+    }
+
+    int ans {};
+    if(w[start] <= W) {
+        int option1 {knapsackHelp(w,v,W-w[start], start+1, end) + v[start]};
+        int option2 {knapsackHelp(w,v,W,start+1, end)};
+        ans = std::max(option1, option2);
+    }else {
+        ans = knapsackHelp(w,v,W,start+1,end);
+    }
+    return ans;
+}
+
+int kanpsackMemoization(int* w, int* v,int W, int start, int end, int** dp) {
+    if(W == 0) {
+        return 0;
+    }
+
+    if(start > end) {
+        return 0;
+    }
+
+    if(dp[W][start] != -1) {
+        return dp[W][start];
+    }
+
+    int ans {};
+    if(w[start] <= W) {
+        int option1 {kanpsackMemoization(w,v,W-w[start], start+1, end, dp) + v[start]};
+        int option2 {kanpsackMemoization(w,v,W,start+1, end, dp)};
+        ans = std::max(option1, option2);
+    }else {
+        ans = kanpsackMemoization(w,v,W,start+1,end, dp);
+    }
+    dp[W][start] = ans;
+    return ans;
+}
+
+int knapsack(int* weight, int* value, int n, int maxWeight) {
+    // return knapsackHelp(weight,value,maxWeight,0,n-1);
+    int** dp {new int*[maxWeight+1]};
+    for(int i{}; i<=maxWeight; i++) {
+        dp[i] = new int[n+1];
+        for(int j{}; j<=n; j++) {
+            dp[i][j] = -1;
+        }
+    }
+    
+    int ans {kanpsackMemoization(weight,value,maxWeight,0,n,dp)};
+
+    for(int i{}; i<=maxWeight; i++) {
+        delete [] dp[i];
+    }
+
+    delete [] dp;
+    return ans;
+}
+
+
+int main() {
+    int n;
+    std::cin >> n;
+    int weights[101];
+    for(int i{}; i<n; i++) {
+        std::cin >> weights[i];
+    }
+    int values[101];
+    for(int i{}; i<n; i++) {
+        std::cin >> values[i];
+    }
+    int maxWeight{};
+    std::cin >> maxWeight;
+    
+    int ans {knapsack(weights,values, n, maxWeight)};
+    std::cout << ans << std::endl;
+}
