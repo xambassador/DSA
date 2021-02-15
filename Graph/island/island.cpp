e44cb7a295a984ea700b832659759e9618e35049
Yash Ramoliya <yashramoliya12@gmail.com>
2021-02-15 12:09:52 +0530

    add island problem and solution

---
+++ Graph/island/island.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+void DFS(bool** edges, int& n, int& sv, bool* visited) {
+    visited[sv] = true;
+    for (int i{}; i<n; i++) {
+        if (i == sv) continue;
+
+        if (edges[sv][i] && !visited[i]) {
+            DFS(edges, n, i, visited);
+        }
+    }
+}
+
+int island(bool** edges, int n) {
+    bool* visited {new bool[n]};
+    for (int i{}; i<n; i++) visited[i] = false;
+    int cnt {};
+    for (int i{}; i<n; i++) {
+        if (!visited[i]) {
+            DFS(edges, n, i, visited);
+            cnt++;
+        }
+    }
+
+    delete [] visited;
+    return cnt;
+}
+
+int main(){
+    int v,e;
+    std::cin >> v >> e;
+
+    bool** edges {new bool*[v]};
+    for (int i{}; i<v; i++) {
+        edges[i] = new bool[v];
+        for (int j{}; j<v; j++) {
+            edges[i][j] = false;
+        }
+    }
+
+    for (int i{}; i<e; i++) {
+        int sv, ev;
+        std::cin >> sv >> ev;
+        edges[sv][ev] = true;
+        edges[ev][sv] = true;
+    }
+    
+    int ans {island(edges, v)};
+    std::cout << ans << std::endl;
+
+    for (int i{}; i<v; i++) {
+        delete [] edges[i];
+    }
+
+    delete [] edges;
+}
