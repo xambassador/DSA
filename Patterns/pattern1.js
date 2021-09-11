b2d4a558407ffdf473ebc7bb625f9e9caa1c9058
Yash Ramoliya <yashramoliya12@gmail.com>
2021-09-11 21:14:58 +0530

    add patterns in JavaScript

---
+++ Patterns/pattern1.js
@@ -0,0 +1,31 @@
+/*
+n = 4
+
+1
+1 2
+1 2 3 
+1 2 3 4
+
+*/
+
+function pattern(n) {
+	let row = 1;
+	let ans = "";
+	while (row <= n) {
+		let val = 1;
+		let col = 1;
+		while (col <= row) {
+			ans += val + " ";
+			val++;
+			col++;
+		}
+
+		ans += "\n";
+		row++;
+	}	
+
+	console.log(ans);
+}
+
+pattern(4);
+pattern(10);
