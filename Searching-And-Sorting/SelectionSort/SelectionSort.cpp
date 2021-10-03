6ddf87f3703d7d0678833286a3e938a27d2eeab1
Yash Ramoliya <yashramoliya12@gmail.com>
2021-10-03 18:11:23 +0530

    Update selection sort. Add Recursive version of selection sort

--- Searching-And-Sorting/SelectionSort/SelectionSort.cpp
+++ Searching-And-Sorting/SelectionSort/SelectionSort.cpp
@@ -1,32 +1,71 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include <algorithm>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::swap;
 
-void SelSort(int* arr, int n){
-    for(int i{};i<n;i++){
-        int min{arr[i]};
-        int minIndex{i};
-        for(int j{i+1};j<n;j++){
-            if(arr[j]<min){
-                min = arr[j];
-                minIndex = j;
-            }
+int findSmallElementIndex(int arr[], int n, int start) {
+    int smallElement {arr[start]}, smallElementIndex {start};
+    for (int i{start+1}; i<n; i++) {
+        if (arr[i] < smallElement) {
+            smallElement = arr[i];
+            smallElementIndex = i;
         }
-        int t{arr[i]};
-        arr[i] = arr[minIndex];
-        arr[minIndex] = t;
+    }
+    return smallElementIndex;
+}
+
+// Recursive Selection sort
+
+// If we call recursion first
+void selectionSortRecursive2(int arr[], int n) {
+    // Base case
+    if (n == 0 || n == 1) return;
+    
+    // Call Recursion
+    selectionSortRecursive2(arr+1, n-1);
+
+    // Doing my job, which is find smallest element and swap it with my element
+    int smallElementIndex {findSmallElementIndex(arr, n, 0)};
+    swap(arr[0], arr[smallElementIndex]);
+
+    // Call recursion
+    selectionSortRecursive2(arr+1, n-1);
+}
+
+// If i do my job first.
+void selectionSortRecursive(int arr[], int n) {
+    // Base case
+    if (n == 0 || n == 1) return;
+    
+    // Doing my job, which is find smallest element and swap it with my element
+    int smallElementIndex {findSmallElementIndex(arr, n, 0)};
+    swap(arr[0], arr[smallElementIndex]);
+
+    // Call recursion
+    selectionSortRecursive(arr+1, n-1);
+}
+
+
+// Iterative solution : O(n^2)
+void selectionSort(int arr[], int n) {
+    for (int i{}; i<n; i++) {
+        int smallElementIndex {findSmallElementIndex(arr,n,i)};
+        swap(arr[i], arr[smallElementIndex]);
     }
 }
 
-int main(){
-    int n;
+int main () {
+    int n{}, arr[1000000]{};
     cin >> n;
-    int* arr{new int[n]};
-    for(int i{};i<n;i++){
-        cin >> arr[i];
-    }
-    SelSort(arr,n);
-    for(int i{};i<n;i++){
-        cout << arr[i] << " ";
-    }cout << endl;
-    delete arr;
+
+    for (int i{}; i<n; i++) cin >> arr[i];
+
+    // selectionSort(arr, n);
+    selectionSortRecursive(arr, n);
+    // selectionSortRecursive2(arr, n);
+
+    for (int i{}; i<n; i++) cout << arr[i] << " ";
+
 }
