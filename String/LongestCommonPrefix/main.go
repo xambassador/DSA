923a0da66e8addb4c3e097e31f16a915bbac6a4f
Yash Ramoliya <yashramoliya12@gmail.com>
2023-03-30 17:28:58 +0530

    Add solution of Longest common prefix

---
+++ String/LongestCommonPrefix/main.go
@@ -0,0 +1,20 @@
+package main
+
+func longestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+	if len(strs) == 1 {
+		return strs[0]
+	}
+	var prefix string
+	for i := 0; i < len(strs[0]); i++ {
+		for j := 1; j < len(strs); j++ {
+			if i >= len(strs[j]) || strs[j][i] != strs[0][i] {
+				return prefix
+			}
+		}
+		prefix += string(strs[0][i])
+	}
+	return prefix
+}
